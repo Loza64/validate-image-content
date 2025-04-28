34ce17c893b50cfc1295c46915a30a43782ab0fe
Roberto Loza <robertoloxa88@gmail.com>
2025-04-27 19:17:45 -0600

    remove code

--- src/libraries/multer.ts
+++ src/libraries/multer.ts
@@ -4,4 +4,3 @@ const memoryStorage = multer.memoryStorage();
 const upload = multer({ storage: memoryStorage });
 
 export const uploadFile = upload.single("image");
-export const uploadFiles = upload.array("images", 10);
